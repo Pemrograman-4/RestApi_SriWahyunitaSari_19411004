55f63e465797e8f3b7a01a38b6953e4efc073062
sriwahyunita6 <sri.19411004@student.ubl.ac.id>
2021-10-25 14:43:22 +0700

    Rest Api

---
+++ Router/user_router.js
@@ -0,0 +1,19 @@
+/* Bandar Lampung
+10-10-21
+Sri Wahyunita Sari
+NPM 19411004
+*/
+const router = require('express').Router()
+const userController = require('../Controller/user_controller')
+router.post('/sign-up', (req, res) => {
+  userController.register(req.body)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+router.post('/sign-in', (req, res) => {
+  userController.login(req.body)
+  .then(result => res.json(result))
+  .catch(err => res.json(err))
+})
+module.exports = router
